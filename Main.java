593c98e2138138509320facfba3e95405dc19f2a
AnantChhetri <anantchhetri12@gmail.com>
2025-09-25 14:11:50 +0530

    management

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library();  // Create Library instance
+        new LibraryGUI(library);          // Launch GUI
+    }
+}
